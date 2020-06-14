68879226a6614ddc94bcb0cae85f407db882044e
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-14 05:13:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,4 +33,13 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 end
